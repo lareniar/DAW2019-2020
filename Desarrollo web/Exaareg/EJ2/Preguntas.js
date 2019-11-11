0f046d7178aee410e5c75fbdc632269fce0ea965
Aritz <aritz_egiguren@hotmail.com>
2019-11-11 12:18:32 +0100

    Examen

---
+++ Desarrollo web/Exaareg/EJ2/Preguntas.js
@@ -0,0 +1,26 @@
+// JS Document
+var iPreguntas=1;
+var aPreguntas=new Array();
+var aRespuestas=new  Array();
+var aSoluciones=new Array();
+
+aPreguntas[0]="�Cual es el animal m�s rapido del planeta?";
+aRespuestas[0]=new Array("Oso","Guepardo","Tigre","Elefante");
+aSoluciones[0]="1";
+
+aPreguntas[1]="�Cual es la capital de Francia?";
+aRespuestas[1]=new Array("Ulan Bator","Donosti","Paris","Lisboa");
+aSoluciones[1]="2";
+
+aPreguntas[2]="¿Cuál es el animal más largo del planeta?";
+aRespuestas[2]=new Array("Humano","Pajaro","Pitón","Cocodrilo");
+aSoluciones[2]="3";
+
+aPreguntas[3]="�Cual es la capital de Francia?";
+aRespuestas[3]=new Array("Ulan Bator","Donosti","Paris","Lisboa");
+aSoluciones[3]="2";
+
+aPreguntas[4]="�Cual es la capital de Francia?";
+aRespuestas[4]=new Array("Ulan Bator","Donosti","Paris","Lisboa");
+aSoluciones[4]="2";
+
