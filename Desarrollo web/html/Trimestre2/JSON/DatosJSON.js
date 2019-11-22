79e2907178b30fa12896abd56c957b513f5b686f
Aritz <aritz_egiguren@hotmail.com>
2019-11-22 10:37:08 +0100

    JQuery & Trabajo Web

---
+++ Desarrollo web/html/Trimestre2/JSON/DatosJSON.js
@@ -0,0 +1,6 @@
+
+var DatosJSON = '[';
+DatosJSON += '{"nombre":"Itziar","edad":31,"ciudad":"Irun"},';
+DatosJSON += '{"nombre":"Pepe","edad":21,"ciudad":"Donosti"},';
+DatosJSON += '{"nombre":"Carlos","edad":25,"ciudad":"Astigarraga"}';
+DatosJSON += ']';
