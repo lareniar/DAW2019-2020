a62d8673ef36bc3cc1c8baae7834f4a20833bb20
Aritz <aritz_egiguren@hotmail.com>
2019-12-04 13:28:10 +0100

    Actualizacion 4/12

--- Diseño de interfaces web/EjercicioLibro/script.js
+++ Diseño de interfaces web/EjercicioLibro/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $("button").click(function () {
-      $("*").css('color', 'red');;
+    $(".imgGridHome").click(function () {
+      window.location = "https://www.barnesandnoble.com/b/series/naruto-series/_/N-2k2o"; 
     });
+
+    $("#trayectoria").click(function () {
+      window.open("trayectoria.html");; 
+    });
+
+    $("#biografia").click(function () {
+      window.open("biografia.html");; 
+    });
+
+
+
+
+
 });
+
