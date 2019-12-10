9eed4a4a16f906522bad4525d9770035b060bd97
Aritz <aritz_egiguren@hotmail.com>
2019-12-10 08:51:53 +0100

    Actualizacion archivos

---
+++ Diseño de interfaces web/Trimestre2/EjercicioLibro/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".imgGridHome").click(function () {
+      window.location = "https://www.barnesandnoble.com/b/series/naruto-series/_/N-2k2o"; 
+    });
+
+    $("#trayectoria").click(function () {
+      window.open("trayectoria.html");; 
+    });
+
+    $("#biografia").click(function () {
+      window.open("biografia.html");; 
+    });
+
+
+
+
+
+});
+
